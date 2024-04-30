51489d8d3aef53b9f29455007e6bf24ba2cebb28
Libannur <99373712+Libannur@users.noreply.github.com>
2024-04-30 14:19:40 -0500

    updated Card and added form

--- components/card.js
+++ components/card.js
@@ -1,16 +1,27 @@
-export const  players = (array) => {
-  let domString = ""
-  array.forEach((player) => {
-    domString += `<div class="card mb-3" style="max-width: 540px;">
-    <div class="row g-0">
-      <div class="col-md-8">
-        <div class="card-body">
-          <h5 class="card-title">${player.name}</h5>
-          <p class="card-text">House: ${player.house}</p>
-          <a href="#" class="btn btn-danger" id="delete--${player.id}">Expel</a>
-        </div>
+
+export const  Card = (Object) => {
+  return `<div card-body mb-3 >
+  <div class="row sport-card">
+    <div class="col-md-6">
+      <div class="card-body text-center">
+        <h5 class="card-title">${Object.name}</h5>
+        <p class="card-text">Sport: ${Object.sport}</p>
+        <a href="#" class="btn btn-danger" id="delete--${Object.id}">Expel</a>
       </div>
     </div>
-  </div>`;
-  })
+  </div>
+  </div>
+`;
 }
+
+export const Form =  `<div class="card">
+  <div class="card-body form-body">
+  <form>
+  Player Name:<div class="form-floating mb-3">
+  <input type="text" class="form-control" id="playerName" placeholder="studentName" required>
+  <label for="playerName">Name</label>
+  <button type="submit" class="btn draft-btn btn-primary">Draft</button>
+  </form>
+  </div>
+  </div>
+  </div>`;
