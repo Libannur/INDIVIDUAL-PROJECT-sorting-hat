257ccb3d0ff124525bf574707f40faba0cf4fb84
Libannur <99373712+Libannur@users.noreply.github.com>
2024-04-29 21:52:51 -0500

    declared variables

--- scrips/main.js
+++ scrips/main.js
@@ -1,4 +1,22 @@
-const renderHats = (array) => {
-    let domstring = ""
-   array.forEach(item =)
+import { card } from "../components/card.js"
+import { refercenceList } from "../data/reference.js"
+import { renderToDom } from "../utils/renderToDom.js"
+
+const draftApp = document.querySelector("#app");
+const introBtn = document.querySelector("#introBtn");
+const targetApp = document.querySelector("app");
+const drafted = document.querySelector("#draftedPlayers");
+
+
+const startApp = () => {
+     draftApp.toggleAttribute("hidden")
 }
+
+const renderCards = (array) => {
+    let domString = "";
+    array.forEach( item => {
+        domString.inne += targetApp(item)
+    } )
+    renderToDom("app", domString)
+}
+introBtn.addEventListener("click", renderCards)
