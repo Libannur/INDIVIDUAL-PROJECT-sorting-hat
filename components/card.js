79ec87e959afaeedbe1c96688214aa8526c0ea0c
Libannur <99373712+Libannur@users.noreply.github.com>
2024-05-01 09:01:12 -0500

    updated card and moved renderForm to main.js

--- components/card.js
+++ components/card.js
@@ -1,6 +1,7 @@
 
 export const  Card = (Object) => {
-  return `<div card-body mb-3 >
+  return `
+  <div card-body mb-3 >
   <div class="row sport-card">
     <div class="col-md-6">
       <div class="card-body text-center">
@@ -14,34 +15,4 @@ export const  Card = (Object) => {
 `;
 }
 
-export const renderForm =  () => {
-    let domString = ""
-  domString +=`<div class="card">
-    <div class="card-body form-body">
-    <form>
-    Player Name:<div class="form-floating mb-3">
-    <input type="text" class="form-control" id="playerName" placeholder="playerName" required>
-    <label for="playerName">Name</label>
-    <button type="submit" class="btn draft-btn btn-primary" id="sort-btn">Sort</button>
-    </form>
-    </div>
-    </div>
-    </div>`;
-    const formDiv = document.querySelector("#playerForm")
-    formDiv.innerHTML = domString;
-    const form = document.querySelector("form")
-
-    console.log(form);
-    
-    form.addEventListener("submit", (event) => {
-    event.preventDefault()
-    
-    const newPlayer = {
-        id: players.length + 1,
-        name: document.querySelector("#playerName").value
-    }
-    console.log(newPlayer.name);
-    })
-}
-   
 
