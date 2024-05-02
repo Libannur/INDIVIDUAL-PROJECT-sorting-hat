607846e1f5b2106338405c6e75b5fd48a53d1241
Libannur <99373712+Libannur@users.noreply.github.com>
2024-05-02 10:53:48 -0500

    filtered buttons

--- scrips/main.js
+++ scrips/main.js
@@ -2,10 +2,19 @@ import {  Card } from "../components/card.js"
 import { players } from "../data/reference.js"
 import { renderToDom } from "../utils/renderToDom.js"
 
+
+// Declare key varibales to access the dom
 const introBtn = document.querySelector("#intro-btn")
 const draftedPlayers = document.querySelector("#draftedPlayers")
 const sortBtn = document.querySelector("#sort-btn")
 const formDiv = document.querySelector("#playerForm")
+const showAll = document.querySelector("#all");
+const soccer = document.querySelector("#soccer");
+const football = document.querySelector("#football");
+const basketball = document.querySelector("#basketball");
+const baseball = document.querySelector("#baseball");
+
+
 
 
 // a Function that puts the card on the dom.
@@ -19,14 +28,17 @@ const renderCard = (array) => {
 
 
 
+// create a fucntion that random selects sports for the new-Obj
 const randomise = () => {
     const sport = ["soccer", "football", "basketball", "baseball"]
     const randomSport = Math.floor(Math.random() * sport.length)
     return sport[randomSport]
 }
 
+console.log("random Sporst:", randomise());
 
-console.log(randomise());
+
+// create a function that creates form and render it to dom
 const renderForm =  () => {
     const formDiv = document.querySelector("#playerForm")
     
@@ -54,8 +66,7 @@ const renderForm =  () => {
             name: document.querySelector("#playerName").value,
             sport: randomise()
         };
-        console.log(newPlayerObj.name);
-        console.log(newPlayerObj.id);
+        console.log("new player:", newPlayerObj.name);
         
         players.push(newPlayerObj)
         renderCard(players)
@@ -63,10 +74,47 @@ const renderForm =  () => {
     })
 }
 
+// create Button Filter 
+
+const buttonFilter = (array, sportString) => {
+    let newSportArray = []
+  for (const player of players) {
+    if(player.sport === sportString) {
+        newSportArray.push(player)
+    }
+    console.log(newSportArray);
+  }
+  renderCard(newSportArray)
+}
+
+
+
 const starApp = () => {
 
     introBtn.addEventListener("click", renderForm) 
     renderCard(players)
+    
+    showAll.addEventListener("click", () => {
+        renderCard(players)
+    } )
+    
+    soccer.addEventListener("click", () => {
+        buttonFilter(players, "soccer")
+    })
+    
+    basketball.addEventListener("click", () => {
+        buttonFilter(players, "basketball")
+    })
+    baseball.addEventListener("click", () => {
+        buttonFilter(players, "baseball")
+    })
+    football.addEventListener("click", () => {
+        buttonFilter(players, "football")
+    })
 }
+
+    
+
+
 starApp()
     
